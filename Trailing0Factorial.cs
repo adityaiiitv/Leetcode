5494924b5ab1ee7a06e7d3ce421213f02fb59062
Aditya Prakash <aditya.prakash101194@gmail.com>
2019-07-03 15:41:51 -0500

    Trailing 0 Factorial

---
+++ Trailing0Factorial.cs
@@ -0,0 +1,10 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int r = 0;
+        while (n > 0) {
+            n /= 5;
+            r += n;
+        }
+        return r;
+    }
+}
