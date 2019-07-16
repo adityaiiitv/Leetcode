372aa048ffd01ec7ed207caf791270e4167ab716
Aditya Prakash <aditya.prakash101194@gmail.com>
2019-07-16 13:13:07 -0500

    My Sqrt

---
+++ MySqrt.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0)
+        {
+            return 0;
+        }
+        int left = 1, right = Integer.MAX_VALUE, mid;
+        while(true)
+        {
+            mid = left+(right-left)/2;
+            if(mid > x/mid)
+            {
+                right = mid-1;
+            }
+            else{
+                if((mid+1)>x/(mid+1))
+                {
+                    return mid;
+                }
+                else{
+                    left = mid+1;
+                }
+            }
+        }
+    }
+}
